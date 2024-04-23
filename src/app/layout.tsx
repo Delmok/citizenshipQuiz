650fee7929c5a06555521e44542e9d530b41a5a8
gank <meatbagtv@gmail.com>
2024-04-23 14:52:17 -0400

    adding adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,9 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={inter.className}>{children}</body>
-    <GoogleAnalytics gaId="G-2YWB4L05HV" />
-    <GoogleTagManager gtmId="GTM-TLQW6HXG" />
+        <head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9722437436108814"
+        crossOrigin="anonymous"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        <GoogleAnalytics gaId="G-2YWB4L05HV" />
+        <GoogleTagManager gtmId="GTM-TLQW6HXG" />
     </html>
   );
 }
